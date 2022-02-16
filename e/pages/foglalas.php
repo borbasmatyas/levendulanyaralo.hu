ac3c368b358b74974d70e31515e03735450eabab
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-02-16 17:08:23 +0100

    Update

--- e/pages/foglalas.php
+++ e/pages/foglalas.php
@@ -1,12 +1,6 @@
-<div style="clear:both;">
-<!-- Naptár -->
-<iframe width="100%" height="600" src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FBudapest&title=Foglalts%C3%A1g&showNav=1&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTitle=0&src=NXBna2pyMGpncWtuMGtraW1vMDB0bGlqNW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23E67C73" style="border-width:0" frameborder="0" scrolling="no"></iframe>
-<!--/Naptár -->
-</div>
-
 <p>Az árak az idegenforgalmi adót NEM tartalmazzák !</p>
 
-<div style="clear:both;">
+
   <table>
     <tr>
       <td>Áraink</td>
@@ -28,10 +22,23 @@
 
   </table>
 
-</div>
+
+
+
+<!-- Naptár -->
+  <iframe width="100%" height="600" src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FBudapest&title=Foglalts%C3%A1g&showNav=1&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTitle=0&src=NXBna2pyMGpncWtuMGtraW1vMDB0bGlqNW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23E67C73" style="border-width:0" frameborder="0" scrolling="no"></iframe>
+<!--/Naptár -->
 
 
 <p>Az online szobafoglalás elküldése nem jelenti a biztos foglalást, ezáltal csak az igényét juttatja el hozzánk. A foglalás az előleg beérkezésével, és írásos visszaigazolással válik véglegessé! Kérjük, adja meg pontosan minden adatát! Adatait bizalmasan kezeljük, harmadik fél számára nem adjuk ki!</p>
-<!-- Foglalási űrlap -->
-<iframe width="100%" height="1800" src="https://docs.google.com/forms/d/e/1FAIpQLSercNM5-9PauO8vxgPQtL-Bk6Npz1jlsf-P1mvaGgj37Ukz2g/viewform?embedded=true"  frameborder="0" marginheight="0" marginwidth="0">Betöltés…</iframe>
-<!--/Foglalási űrlap -->
+
+<a href="#urlap"class="hide">Foglalási űrlap megnyitása</a>
+<a href="#urlap"class="show">Foglalási űrlap elrejtése</a>
+
+<div id="urlap" class="urlap">
+
+  <!-- Foglalási űrlap -->
+    <iframe width="100%" height="1800" src="https://docs.google.com/forms/d/e/1FAIpQLSercNM5-9PauO8vxgPQtL-Bk6Npz1jlsf-P1mvaGgj37Ukz2g/viewform?embedded=true"  frameborder="0" marginheight="0" marginwidth="0">Betöltés…</iframe>
+  <!--/Foglalási űrlap -->
+
+</div>
