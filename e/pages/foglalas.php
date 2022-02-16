bf5d5df05e28747d5e77b49c20d6b886948cdee0
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-02-16 16:36:24 +0100

    Update foglalas.php

--- e/pages/foglalas.php
+++ e/pages/foglalas.php
@@ -1,6 +1,6 @@
 <div style="clear:both;">
 <!-- Naptár -->
-<iframe jsname="L5Fo6c" class="YMEQtf KfXz0b" sandbox="allow-scripts allow-popups allow-forms allow-same-origin allow-popups-to-escape-sandbox allow-downloads allow-modals" aria-label="Calendar" src="https://www.google.com/calendar/embed?color=%23668CD9&amp;deb=-&amp;embed_style=WyJhdDplbWI6c3QiLCIjZTBlMGUwIiwiI2VkZWRlZCIsIiM0MTg0ZjMiLCJyb2JvdG8iLCIjNjM2MzYzIiw1MDAsIiNmZmYiXQo&amp;eopt=0&amp;mode=agenda&amp;showCalendars=1&amp;showPrint=0&amp;showTz=0&amp;src=5pgkjr0jgqkn0kkimo00tlij5o@group.calendar.google.com" allowfullscreen="" frameborder="0"></iframe>
+<iframe width="100%" height="500px" jsname="L5Fo6c" class="YMEQtf KfXz0b" sandbox="allow-scripts allow-popups allow-forms allow-same-origin allow-popups-to-escape-sandbox allow-downloads allow-modals" aria-label="Calendar" src="https://www.google.com/calendar/embed?color=%23668CD9&amp;deb=-&amp;embed_style=WyJhdDplbWI6c3QiLCIjZTBlMGUwIiwiI2VkZWRlZCIsIiM0MTg0ZjMiLCJyb2JvdG8iLCIjNjM2MzYzIiw1MDAsIiNmZmYiXQo&amp;eopt=0&amp;mode=agenda&amp;showCalendars=1&amp;showPrint=0&amp;showTz=0&amp;src=5pgkjr0jgqkn0kkimo00tlij5o@group.calendar.google.com" allowfullscreen="" frameborder="0"></iframe>
 <!-- / Naptár -->
 </div>
 
