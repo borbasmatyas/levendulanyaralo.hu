763450e62d965df8907a72cdbde661b5fb11e834
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-02-17 09:06:53 +0100

    Update kepek.php

---
+++ e/pages/kepek.php
@@ -0,0 +1,15 @@
+<article>
+<h1>Képek</h1>
+
+<img src="/images/kep_05.jpg" alt="Terasz" width="100%">
+
+<img src="/images/kep_04.jpg" alt="Kert" width="100%">
+
+<img src="/images/kep_06.jpg" alt="Belső tér" width="100%">
+
+<img src="/images/kep_07.jpg" alt="Belső tér" width="100%">
+
+<img src="/images/kep_08.jpg" alt="Kerti játékok" width="100%">
+
+
+</article>
