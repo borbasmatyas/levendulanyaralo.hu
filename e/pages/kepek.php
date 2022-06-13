5b0e1f037fc355f120fd62222ce7646d3bc782ff
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-06-13 10:17:31 +0200

    Update kepek.php

--- e/pages/kepek.php
+++ e/pages/kepek.php
@@ -2,19 +2,16 @@
   
   <h1>Képek</h1>
 
-  <img src="/images/kep_05.jpg" alt="Terasz" width="100%">
+<?php
+$i=1;
+$max=18;
 
-  <img src="/images/kep_04.jpg" alt="Kert" width="100%">
-
-  <img src="/images/kep_06.jpg" alt="Belső tér" width="100%">
-
-  <img src="/images/kep_07.jpg" alt="Belső tér" width="100%">
-
-  <img src="/images/kep_09.jpg" alt="Ágy" width="100%">
-
-  <img src="/images/kep_08.jpg" alt="Kerti játékok" width="100%">
-
-  <img src="/images/kep_10.jpg" alt="Kerti pad" width="100%">
-
+while($i <= $max) {
+  echo <<<HTML
+    <img src="/images/kep_{$i}.jpg" width="100%">>;
+  HTML;
+  $i++;
+}
+  ?>
 
 </article>
